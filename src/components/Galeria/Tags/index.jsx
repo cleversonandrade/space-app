e8a21aa0ca31638f6f67eb81ae8406dbd06cf4bd
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2023-11-21 21:36:42 -0300

    fim do projeto

---
+++ src/components/Galeria/Tags/index.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+import tags from './tags.json'
+
+const BarrasTags = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 64px;
+    margin-top: 56px;
+`
+
+const TituloTags = styled.p`
+    color: #D9D9D9;
+    font-size: 24px;
+    margin: 0;
+`
+
+const Tag = styled.button`
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+        border-color:  #C98CF1;
+    }
+`
+
+const Div = styled.div`
+    display: flex;
+    gap: 24px;
+    justify-content: end;
+`
+
+const Tags = ({ setTag }) => {
+    return (
+        <BarrasTags>
+        <TituloTags>Busque por tags:</TituloTags>
+        <Div>
+            {tags.map(tag => <Tag key={tag.id} onClick={() => setTag(tag.tag)}>{tag.titulo}</Tag>)}
+        </Div>
+    </BarrasTags>)
+}
+
+export default Tags
